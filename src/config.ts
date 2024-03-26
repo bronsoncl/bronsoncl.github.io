628ed4e1258f16208f875979b9cf776b7a7e953e
Bronson Lim <BLim@csusb.edu>
2024-03-26 16:26:24 -0700

    first attempt

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dr. Bronson Lim | Academic Website';
+export const SITE_DESCRIPTION = 'This is the academic website of Dr. Bronson Lim. He is an assistant professor of mathematics at California State University San Bernardino';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
